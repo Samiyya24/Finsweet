07e0aaed23c385326c3eb05c2494168fc2138ec6
Samiyya24 <samiyyaazizkhanova904@gmail.com>
2024-06-04 23:23:24 +0500

    test

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,20 +4,20 @@
     <div class="col-span-2 row-span-2 relative overflow-hidden group">
       <img class="w-full h-full" src="/img/Card.png" alt="" />
       <div
-        class="absolute bg-gradient-to-br from-[#1C1E536B]/40 to-[#1C1E53] top-0 w-1/2 -left-full h-full text-white group-hover:left-0 duration-1000"
+        class="absolute bg-gradient-to-br from-[#1C1E536B]/40 to-[#1C1E53] top-0 w-1/2 md:-left-full h-full text-white md:group-hover:left-0 duration-1000"
       >
         <div class="flex flex-col gap">
-          <div class="max-w-[400px] h-[400px]"></div>
-          <div class="max-w-1/2 text-left px-9">
-            <h3 class="text-2xl font-semibold mb-4">
+          <div class="max-md:hidden max-w-[400px] h-[400px]"></div>
+          <div class="max-w-1/2 text-left md:px-9 px-2">
+            <h3 class="md:text-2xl font-semibold mb-4">
               Workhub office Webflow Webflow Design
             </h3>
-            <p class="text-white/80 mb-10">
+            <p class="text-white/80 mb-10 max-md:text-xs">
               Euismod faucibus turpis eu gravida mi. Pellentesque et velit
               aliquam
             </p>
             <router-link
-              class="flex items-center gap-x-4 cursor-pointer text-[#FCD980]"
+              class="flex items-center max-md:text-sm md:gap-x-4 gap-2 cursor-pointer text-[#FCD980]"
               to="/pricing"
               >View Pricing
               <svg
@@ -39,10 +39,10 @@
     <div class="relative overflow-hidden group">
       <img class="w-full h-full" src="/img/img6.png" alt="" />
       <div
-        class="absolute px-14 bg-gradient-to-br from-[#1C1E536B]/40 to-[#1C1E53] top-0 group-hover:w-full w-1/2 -left-full h-full text-white group-hover:left-0 duration-1000"
+        class="absolute px-14 bg-gradient-to-br from-[#1C1E536B]/40 to-[#1C1E53] top-0 md:group-hover:w-full md:w-1/2 w-full -left-full h-full text-white group-hover:left-0 duration-1000"
       >
         <div class="flex flex-col">
-          <div class="w-60 h-32"></div>
+          <div class="w-60 md:h-32 h-20"></div>
           <div>
             <h3 class="text-2xl font-semibold mb-4 max-w-48">
               Unisaas Website Design
@@ -73,7 +73,7 @@
         class="absolute px-14 bg-gradient-to-br from-[#1C1E536B]/40 to-[#1C1E53] top-0 group-hover:w-full w-1/2 -left-full h-full text-white group-hover:left-0 duration-1000"
       >
         <div class="flex flex-col">
-          <div class="w-60 h-40"></div>
+          <div class="w-60 md:h-40 h-20"></div>
           <div>
             <h3 class="text-2xl font-semibold mb-4 max-w-48">
               Unisaas Website Design
