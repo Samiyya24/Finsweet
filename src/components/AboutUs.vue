af7a4da2b2328f3daa5252bf90b49ec03ffbc97e
Samiyya24 <samiyyaazizkhanova904@gmail.com>
2024-06-05 10:47:49 +0500

    test

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,12 +1,12 @@
 <script setup></script>
 <template>
-  <div class="flex pb-32 pt-64 container items-center gap-40">
-    <div class="text-primary">
+  <div class="md:flex pb-32 md:pt-64 pt-24 container items-center gap-40">
+    <div class="text-primary max-md:text-center">
       <span class="text-lg mb-2">About us</span>
-      <h1 class="text-6xl font-semibold mb-8 leading-[74px]">
+      <h1 class="md:text-6xl text-4xl font-semibold mb-8 md:leading-[74px]">
         Our designs solve problems
       </h1>
-      <p class="leading-7 text-primary/70">
+      <p class="leading-7 text-primary/70 max-md:mb-5">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam.
